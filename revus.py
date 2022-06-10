223bc426ee4eafbd6a5bb63b455551ee78ce305c
Katilmami <93917818+Katilmami@users.noreply.github.com>
2022-06-10 16:50:47 +0300

    Add files via upload

---
+++ revus.py
@@ -0,0 +1,25 @@
+import pyautogui as auto
+import time
+
+print("Revus OwO Bot | 5 Saniye Sonra Aktif Edilecektir.")
+print("----------------------------------------------------")
+print("Developer > Athenax#0001, Lebronze James §#9461 ")
+print("----------------------------------------------------")
+print("Bu Bot Python İle Geliştirilmiştir.")
+time.sleep(5)
+x=0
+while True:
+    auto.write("owo h")
+    auto.press("Enter")
+    time.sleep(4)
+    auto.write("owo b")
+    auto.press("Enter")
+    time.sleep(15)
+    x+=1
+    if x == 4:
+        x=0
+        auto.write("Revus Code OwO Bot | 1 Dakıkalıgına Durdurulmustur!")
+        auto.press("Enter")
+        time.sleep(60)
+        auto.write("Revus Code OwO Bot | Yeniden Baslatılmıstır!")
+        auto.press("Enter")
